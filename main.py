e3ea459f917560bb892dbbcb3a8a1d5c25073974
TusharDhyani <dhyanitushar@gmail.com>
2022-07-10 11:57:48 +0200

    Json integration for config

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 # AudioSet Model : https://github.com/kkoutini/passt
-import torch
 
-import numpy as np
-import librosa
+import json
 import cv2
+import numpy as np
+from typing import List
 import random
-
+import torch
 import ffmpeg
+
+import librosa
 import soundfile as sf
 from src.beat_detection2 import find_onesets
 from src.helper import findAcoustics, enhanceAudios
 from src.render import render_video, render_audio
 
 import pdb
-random.seed(10) # 10 for wonderful world # 1 for sleepwalk
 
+random.seed(10)  # 10 for wonderful world # 1 for sleepwalk
 
-
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
 
 # find beats in the audio
 def findBeats(y, sr, sec_thres, max_peaks):
@@ -25,36 +30,43 @@ def findBeats(y, sr, sec_thres, max_peaks):
     out = find_onesets(y, sr, sec_thres, max_peaks)
     beats = np.zeros(len(y))
     for each in out:
-        if(each < sec_thres):
+        if each < sec_thres:
             continue
-        beats[int(each*sr)] = 1
+        beats[int(each * sr)] = 1
     return beats
 
 
-def main():
+def main(
+    video_locations: List = None,
+    audio_input: str = None,
+):
 
+    print(video_locations, audio_input)
+    if video_locations == None or audio_input == None:
+        raise "Please add video files list and audio file location."
     # video locations in a list
-    video_locations = [
-        "./videos/1.mp4",
-        "./videos/2.mp4",
-        "./videos/3.mp4",
-        "./videos/4.mp4",
-        "./videos/5.mp4",
-        "./videos/6.mp4",
-        "./videos/bg_speech.mp4"
-        # "./videos/7.mp4", # landscape video
-        # "./videos/bg_noise.mp4",
-        # "./videos/bg_song.mp4",
-        # "./videos/bg_speech.mp4",
-    ]
+    if video_locations is not None:
+        video_locations = [
+            "./videos/1.mp4",
+            "./videos/2.mp4",
+            "./videos/3.mp4",
+            "./videos/4.mp4",
+            "./videos/5.mp4",
+            "./videos/6.mp4",
+            "./videos/bg_speech.mp4"
+            # "./videos/7.mp4", # landscape video
+            # "./videos/bg_noise.mp4",
+            # "./videos/bg_song.mp4",
+            # "./videos/bg_speech.mp4",
+        ]
     random.shuffle(video_locations)
 
     # load input audio
-    y, sr = librosa.load("./sleepwalk.mp3")
+    y, sr = librosa.load(audio_input)
 
     # find beats for the audio in a list
-    
-    sec_thres = 5 # 5 for what a wonderful world  # 4 for sleepwalk
+
+    sec_thres = 5  # 5 for what a wonderful world  # 4 for sleepwalk
     max_peaks = 10
     beats = findBeats(y, sr, sec_thres, max_peaks)
     print(beats)
@@ -82,8 +94,11 @@ def main():
     final_audio = render_audio(video_locations, audios, sr, video_assignments, alpha)
 
     sf.write("output_audio.wav", final_audio, sr, subtype="PCM_24")
-    
+
     print(video_locations)
 
+
 if __name__ == "__main__":
-    main()
+    config = json.load(open("./config.json", "r"))
+    config = AttrDict(config)
+    main(video_locations=config.video, config.audio)
